1767564d832a285b43f528c503fd328bce0a8034
Jeremy Drake <github@jdrake.com>
2025-04-16 12:02:07 -0700

    hack: cygwin: define _GLIBCXX_USE_CXX11_ABI to 1 if not previously defined
    work around llvm/llvm-project#135910
    see also msys2/MSYS2-packages#5329

--- clang/lib/Driver/ToolChains/Cygwin.h
+++ clang/lib/Driver/ToolChains/Cygwin.h
@@ -27,6 +27,11 @@ public:
   void
   AddClangSystemIncludeArgs(const llvm::opt::ArgList &DriverArgs,
                             llvm::opt::ArgStringList &CC1Args) const override;
+
+  void
+  addClangTargetOptions(const llvm::opt::ArgList &DriverArgs,
+                        llvm::opt::ArgStringList &CC1Args,
+                        Action::OffloadKind DeviceOffloadKind) const override;
 };
 
 } // end namespace toolchains
