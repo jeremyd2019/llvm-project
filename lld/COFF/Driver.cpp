4f201c9c2ab0cc7c5a9238796f338f70b5f0832a
jeremyd2019 <github@jdrake.com>
2025-05-02 00:58:14 -0700

    [LLD][COFF] add __{data,bss}_{start,end}__ symbols for Cygwin support (#136180)
    Cygwin requires these symbols for its fork emulation to know what data
    to copy into the child. GNU ld defines these symbols for MinGW targets
    also, so do the same here.
    
    Cygwin also has the `.data_cygwin_nocopy` section, which is merged into
    `.data` outside the `__data_start__` to `__data_end__` range. This
    excludes it from fork's copying. AFAIK it's only used by the Cygwin DLL
    itself (which requires a custom linker script to link, that's not
    supported by LLD), but the section is included in GNU ld's default
    linker script so handle it here too.
    
    Signed-off-by: Jeremy Drake <github@jdrake.com>

--- lld/COFF/Driver.cpp
+++ lld/COFF/Driver.cpp
@@ -2024,6 +2024,7 @@ void LinkerDriver::linkerMain(ArrayRef<const char *> argsArr) {
     parseMerge(".ctors=.rdata");
     parseMerge(".dtors=.rdata");
     parseMerge(".CRT=.rdata");
+    parseMerge(".data_cygwin_nocopy=.data");
   }
 
   // Handle /section
@@ -2480,6 +2481,10 @@ void LinkerDriver::linkerMain(ArrayRef<const char *> argsArr) {
     if (config->mingw) {
       symtab.addAbsolute(symtab.mangle("__CTOR_LIST__"), 0);
       symtab.addAbsolute(symtab.mangle("__DTOR_LIST__"), 0);
+      symtab.addAbsolute("__data_start__", 0);
+      symtab.addAbsolute("__data_end__", 0);
+      symtab.addAbsolute("__bss_start__", 0);
+      symtab.addAbsolute("__bss_end__", 0);
     }
     if (config->debug || config->buildIDHash != BuildIDHash::None)
       if (symtab.findUnderscore("__buildid"))
