b183a2774d0cd93f98c9f1bb0708d4f0762d1d9c
Tomohiro Kashiwada <kikairoya@gmail.com>
2025-05-15 04:22:35 +0900

    [Cygwin] Template instantiations should be exported by default (#139799)
    Behaves as same as both of Clang and GCC targetting MinGW. Required for
    compatibility for Cygwin-GCC.
    
    Divided from https://github.com/llvm/llvm-project/pull/138773

--- clang/lib/Sema/SemaDeclCXX.cpp
+++ clang/lib/Sema/SemaDeclCXX.cpp
@@ -6465,7 +6465,7 @@ void Sema::checkClassLevelDLLAttribute(CXXRecordDecl *Class) {
   // declarations, except in MinGW mode.
   if (ClassExported && !ClassAttr->isInherited() &&
       TSK == TSK_ExplicitInstantiationDeclaration &&
-      !Context.getTargetInfo().getTriple().isWindowsGNUEnvironment()) {
+      !Context.getTargetInfo().getTriple().isOSCygMing()) {
     Class->dropAttr<DLLExportAttr>();
     return;
   }
