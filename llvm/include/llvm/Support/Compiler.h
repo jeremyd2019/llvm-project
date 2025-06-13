1c772d7c05315abea771449504ddebed72dc18b5
Tomohiro Kashiwada <kikairoya@gmail.com>
2025-06-14 05:44:08 +0900

    [Cygwin] Define LLVM_ABI for Cygwin (#143222)
    592243c1cb3ea53b34033132a87b0d14af9d1079 should be also applied to
    LLVM_ABI.

--- llvm/include/llvm/Support/Compiler.h
+++ llvm/include/llvm/Support/Compiler.h
@@ -198,7 +198,7 @@
 #endif
 #define LLVM_ABI_EXPORT __declspec(dllexport)
 #elif defined(__ELF__) || defined(__MINGW32__) || defined(_AIX) ||             \
-    defined(__MVS__)
+    defined(__MVS__) || defined(__CYGWIN__)
 #define LLVM_ABI LLVM_ATTRIBUTE_VISIBILITY_DEFAULT
 #define LLVM_TEMPLATE_ABI LLVM_ATTRIBUTE_VISIBILITY_DEFAULT
 #define LLVM_EXPORT_TEMPLATE
